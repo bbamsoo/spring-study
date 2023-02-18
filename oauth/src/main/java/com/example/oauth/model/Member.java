1152a06a8de789c5c5ec658823900285ec1eafdc
bbamsoo <hiaro66@naver.com>
2023-02-18 21:29:37 +0900

    Feat : mysql 연동

--- oauth/src/main/java/com/example/oauth/model/Member.java
+++ oauth/src/main/java/com/example/oauth/model/Member.java
@@ -1,4 +1,27 @@
 package com.example.oauth.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String role;
 }
